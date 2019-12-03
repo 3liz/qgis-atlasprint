348658e5f7e582436f9cbe14e2f074794a293673
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-12-03 12:39:54 +0100

    replace $id when we can to optimize the feature filter

--- tests/test_atlas.py
+++ tests/test_atlas.py
@@ -1,7 +1,8 @@
 """Test core functions."""
 
 import json
-from qgis.testing import unittest
+
+from qgis.core import QgsVectorLayer
 
 def test_global_scales(client):
 
@@ -29,3 +30,35 @@ def test_not_supported_request(client):
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
 
+
+def test_optimize_filter():
+    """Test we can optimize the feature filter if $id."""
+    from atlasprintServer.core import optimize_expression
+    layer = QgsVectorLayer('None?field=primary:integer&field=name:string(20)', 'test', 'memory')
+
+    # No primary key
+    assert 'abc' == optimize_expression(layer, 'abc')
+    assert '$id=3' == optimize_expression(layer, '$id=3')
+    assert "$id in ('1','2')" == optimize_expression(layer, "$id in ('1','2')")
+
+    # One primary key
+    def fake_keys():
+        return ['primary']
+    layer.primaryKeyAttributes = fake_keys
+
+    assert '"primary"=3' == optimize_expression(layer, '$id=3')
+    assert '"primary" in (\'1\',\'2\')' == optimize_expression(layer, "$id in ('1','2')")
+
+    # Two primary keys
+    def fake_keys():
+        return ['primary', 'name']
+    layer.primaryKeyAttributes = fake_keys
+    assert '$id=3' == optimize_expression(layer, '$id=3')
+
+    layer = QgsVectorLayer('None?field=primary:string(20)&field=name:string(20)', 'test', 'memory')
+
+    # One primary key but it's not integer
+    def fake_keys():
+        return ['primary']
+    layer.primaryKeyAttributes = fake_keys
+    assert '$id=3' == optimize_expression(layer, '$id=3')
