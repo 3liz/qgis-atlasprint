8f31df12431d2cff77e7a9acce282bc7b7e12dca
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-12-04 11:20:14 +0100

    improve tests

--- tests/test_getprintatlas.py
+++ tests/test_getprintatlas.py
@@ -18,92 +18,84 @@ def test_atlas_getprint_failed(client):
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: TEMPLATE is required'
 
     # Make a failed request no EXP_FILTER
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: EXP_FILTER is required'
 
     # Make a failed request with invalid EXP_FILTER (not well formed)
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1&EXP_FILTER=id in (1, 2"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == (
+        'ATLAS - Error from the user while generating the PDF: Expression is invalid: \nsyntax error, unexpected $end, expecting COMMA or \')\'')
 
     # Make a failed request with invalid EXP_FILTER (unknown field)
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1&EXP_FILTER=fakeId in (1, 2)"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: Expression is invalid, eval error: Column \'fakeId\' not found'
 
     # Make a failed request with invalid TEMPLATE (unknown layout)
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=Fakelayout1&EXP_FILTER=id in (1, 2)"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: Layout not found'
 
     # Make a failed request with invalid SCALE and SCALES (can not be used together)
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1&EXP_FILTER=id in (1, 2)&SCALE=5000&SCALES=10000,5000"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: SCALE and SCALES can not be used together.'
 
     # Make a failed request with invalid SCALE
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1&EXP_FILTER=id in (1, 2)&SCALE=5000n"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: Invalid number in SCALE.'
 
     # Make a failed request with invalid SCALES
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=atlas_simple.qgs&TEMPLATE=layout1&EXP_FILTER=id in (1, 2)&SCALES=10000n,5000"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: Invalid number in SCALES.'
 
     # Make a failed request on a project without atlas layout
     projectfile = "france_parts.qgs"
     qs = "?SERVICE=ATLAS&REQUEST=GetPrint&MAP=france_parts.qgs&TEMPLATE=layout1&EXP_FILTER=id in (1, 2)&SCALES=10000,5000"
     rv = client.get(qs, projectfile)
     assert rv.status_code == 400
-
     assert rv.headers.get('Content-Type', '').find('application/json') == 0
-
     b = json.loads(rv.content.decode('utf-8'))
     assert b['status'] == 'fail'
+    assert b['message'] == 'ATLAS - Error from the user while generating the PDF: Layout not found'
 
 
 def test_atlas_getprint(client):
